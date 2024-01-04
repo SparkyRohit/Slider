15ee20eee2a6a121803dd1bd1d328c495289855f
Rohit kumar <rs3126989@gmail.com>
2024-01-04 22:44:03 +0530

    Added React slick perfectly

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sliderslick from './Component/Sliderslick'
 
 const App = () => {
   return (
     <div>
-      
+      <Sliderslick />
     </div>
   )
 }
--- slider/src/Component/Sliderslick.jsx
+++ slider/src/Component/Sliderslick.jsx
@@ -1,10 +1,8 @@
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import React, { Component } from "react";
 import Slider from "react-slick";
 
-export default class Responsive extends Component {
-  render() {
+const Sliderslick=()=> {
     var settings = {
       dots: true,
       infinite: false,
@@ -71,4 +69,4 @@ export default class Responsive extends Component {
       </div>
     );
   }
-}
+export default Sliderslick
